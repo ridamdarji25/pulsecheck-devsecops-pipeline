213b0a3c175e48553ab03579e642f0d3609090a4
ridamdarji25 <ridamdarji2729@gmail.com>
2026-07-24 22:36:35 +0530

    Fix: upgrade EKS cluster version to 1.31 for standard support

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -37,7 +37,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS control plane"
   type        = string
-  default     = "1.30"
+  default     = "1.31"
 }
 
 variable "node_instance_type" {
